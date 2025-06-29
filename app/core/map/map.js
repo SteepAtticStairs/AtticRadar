94b2301da378837b04775dc6429d7fa7a3382b10
SteepAtticStairs <lawlorcube06@gmail.com>
2025-06-29 00:07:13 -0400

    customized openfreemap's dark style

--- app/core/map/map.js
+++ app/core/map/map.js
@@ -1,6 +1,7 @@
+const style_url = 'https://tiles.openfreemap.org/styles/dark';
 const map = new maplibregl.Map({
     container: 'map',
-    style: 'https://tiles.openfreemap.org/styles/dark',
+    style: style_url,
     zoom: 3,
     center: [-97.3302, 38.5667],
     maxZoom: 20,
@@ -12,6 +13,45 @@ const map = new maplibregl.Map({
     attributionControl: true,
     projection: 'mercator',
 });
+
+/**
+ * Changes a couple things about the map style.
+ * Only works on OpenFreeMap's "dark" style.
+ */
+const now = Date.now();
+fetch(style_url, {
+    cache: 'no-store',
+})
+.then(response => response.json())
+.then(style_json => {
+    try {
+        const uppercase_layers = [];
+        for (var i = 0; i < style_json.layers.length; i++) {
+            // if the layer text is uppercase, store it for later
+            if (style_json.layers[i]?.layout?.['text-transform'] == 'uppercase') {
+                uppercase_layers.push(style_json.layers[i]);
+            }
+        }
+
+        // we want these layers to remain uppercase (country names)
+        const whitelist_layers = ['place_country_major', 'place_country_minor', 'place_country_other']
+        for (var x = 0; x < uppercase_layers.length; x++) {
+            const layer_id = uppercase_layers[x].id;
+            if (!whitelist_layers.includes(layer_id)) {
+                // if it's not whitelisted then remove the forced uppercase
+                map.setLayoutProperty(layer_id, 'text-transform', 'none');
+            } else {
+                // if it is whitelisted (e.g. remaining uppercase), brigten the text color a bit
+                map.setPaintProperty(layer_id, 'text-color', 'rgb(200, 200, 200)');
+            }
+        }
+
+        console.log(`Updated map style in ${Date.now() - now} ms`);
+    } catch (error) {
+        // just in case openfreemap changes their style json and breaks this very specific code
+        console.error(error);
+    }
+})
 
 // can't find any other way to collapse the attribution by default
 $('.maplibregl-ctrl-attrib-button').click();
