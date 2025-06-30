f5f3ff8357a59a72d650d10c42387b2e89c983f1
SteepAtticStairs <lawlorcube06@gmail.com>
2025-06-30 13:45:20 -0400

    fixed TDWR artifacts

--- app/radar/plot/calculation_worker.js
+++ app/radar/plot/calculation_worker.js
@@ -1,3 +1,4 @@
+const CheapRuler = require('cheap-ruler');
 const dest_vincenty = require('./dest_vincenty');
 
 module.exports = function (self) {
@@ -19,7 +20,18 @@ module.exports = function (self) {
         for (var i = 0; i < points.length; i += 2) {
             var az = points[i];
             var distance = points[i + 1];
-            var calc = mc(dest_vincenty({lng: lngLat.lng, lat: lngLat.lat}, distance * 1000, az));
+            var calc;
+            // for TDWRs, the distances start right at the radar station, which is 0.
+            // vincenty's formula can't handle these cases very well but cheap ruler can.
+            // the only time cheap ruler is used is the TDWR gates right next to the radar tower,
+            // eveything else is vincenty. WSR-88Ds don't have this problem so they only use vincenty.
+            if (distance == 0) {
+                console.warn('Gate distance is 0');
+                var ruler = new CheapRuler(lngLat.lat, 'kilometers');
+                calc = mc(ruler.destination([lngLat.lng, lngLat.lat], distance, az));
+            } else {
+                calc = mc(dest_vincenty({lng: lngLat.lng, lat: lngLat.lat}, distance * 1000, az));
+            }
             points[i] = calc[0];
             points[i + 1] = calc[1];
         }
