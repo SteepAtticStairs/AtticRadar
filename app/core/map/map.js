5056cc24d57806ace11f3325b0f12be421ae27f4
SteepAtticStairs <lawlorcube06@gmail.com>
2025-07-18 12:09:11 -0400

    update

--- app/core/map/map.js
+++ app/core/map/map.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoidHdhbGtlcjkyIiwiYSI6ImNtY3UzdXplMTBjazMybnBraXI5b3I5aDUifQ.5OAR3xbwBdD9eRn3TCASPA';
+mapboxgl.accessToken = 'pk.eyJ1IjoidHdhbGtlcjkyIiwiYSI6ImNtZDkwaHMwdTAyazkya3BzNXphYWI3a2kifQ.sWYO653OYlYHYc_wOHsd2A';
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/twalker92/cmd90758s006r01s2df82drgf',
